ad9a002e4162d7804991a3b9de78368d94b94e30
Hector M <moralh2@gmail.com>
2019-04-03 03:34:40 -0500

    trying to delete the notes - wip

--- controllers/articles_controller.js
+++ controllers/articles_controller.js
@@ -7,6 +7,7 @@ var router = express.Router()
 // Import the models
 var Article = require('../models/Article')
 var Note = require('../models/Note')
+var mongoose = require("mongoose")
 
 
 router.get("/articles", function (request, response) {
@@ -38,12 +39,15 @@ router.get("/articles/new", function (request, response) {
 
 router.get("/articles/saved", function (request, response) {
     Article.find({ saved: true })
+        // Populate associated notes
+        .populate("notes")
         .then(function (dbArticle) {
             var hbsObject = {
                 saved: true,
                 articles: dbArticle
             };
-            response.render("index", hbsObject);
+            // response.render("index", hbsObject);
+            response.json(hbsObject);
         })
         .catch(function (err) {
             response.json(err);
@@ -93,6 +97,34 @@ router.post("/articles/:id", function (request, response) {
       .catch(function(err) {
         response.json(err);
       });
+  });
+
+  router.get("/articles/:id/clear-notes", function (request, response) {
+
+    Article.findById(request.params.id)
+    .then(function (dbArticle) {
+        response.json(dbArticle);
+
+        // console.log(dbArticle.notes[0])
+        
+        Note.deleteOne({_id:   mongoose.Types.ObjectId( dbArticle.notes[0] ) })
+        .then(function(dbArticle) {
+        // If update succeeds, send back
+        response.json(dbArticle);
+      })
+
+        // var hbsObject = {
+        //     articles: dbArticle
+        // };
+        // response.render("index", hbsObject);
+        // response.json(hbsObject);
+
+    })
+    .catch(function (err) {
+        response.json(err);
+    });
+
+
   });
 
 
